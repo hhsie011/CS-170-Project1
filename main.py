6c66ea1f10dcf23fd0423d621ff704fe22a89ac1
hhsie011 <sean19990707@gmail.com>
2022-04-27 21:08:17 -0700

    finished building main driver code

--- main.py
+++ main.py
@@ -1,32 +1,60 @@
 #170 project 1
-import math
-
+import ast
+import puzzle as p
 
 
 
 # Create default puzzle
+if __name__ == "__main__":
+    # Welcome message
+    print("Welcome to 862073206 8 puzzle solver.")
 
-# Welcome message
-print("Welcome to 862073206 8 puzzle solver.")
+    # Ask user how to build puzzle
+    pmode = ast.literal_eval(input("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle."))
+    
+    # Instantitate a puzzle object
+    problem = p.Puzzle()
 
-# Create puzzle
-pmode = eval(input("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle."))
-while pmode != 1 and pmode != 2:
-    pmode = eval(input("Error: invalid input, please try again"))
+    # Default puzzle
+    if pmode == '1':
+        problem.build_default_table()
+    # Custom puzzle
+    elif pmode == '2':
+        print("Enter your puzzle, use a zero to represent the blank")
 
-# Default puzzle
-if pmode == '1':
-    temp = 0
-# Custom puzzle
-elif pmode == '2':
-    print("Enter your puzzle, use a zero to represent the blank")
-    print("Enter the frist row, use space or tabs between numbers", end='')
-    print("Enter the second row, use space or tabs between numbers", end='')
-    print("Enter the third row, use space or tabs between numbers", end='')
+        # Ask user for first row
+        print("Enter the frist row, use space or tabs between numbers", end='')
+        
+        # Build first row
+        input1, input2, input3 = int(ast.literal_eval(input.split()))
+        problem.build_custom_table(0, 0, input1)
+        problem.build_custom_table(0, 1, input2)
+        problem.build_custom_table(0, 2, input3)
 
-# Select algorithm
-print("Enter your choice of algorithm")
-print("1: Uniform Cost Search")
-print("2: A* with the Misplaced Tile heuristic")
-print("3: A* with the Eucledian Distance heuristic")
-amode = eval(input())
+        # Ask user for second row
+        print("Enter the second row, use space or tabs between numbers", end='')
+
+        # Build second row
+        input4, input5, input6 = int(ast.literal_eval(input.split()))
+        problem.build_custom_table(1, 0, input4)
+        problem.build_custom_table(1, 1, input5)
+        problem.build_custom_table(1, 2, input6)
+
+        # Ask user for third row
+        print("Enter the third row, use space or tabs between numbers", end='')
+
+        # Build third row
+        input7, input8, input9 = int(ast.literal_eval(input.split()))
+        problem.build_custom_table(2, 0, input7)
+        problem.build_custom_table(2, 1, input8)
+        problem.build_custom_table(2, 2, input9)
+
+    # Select algorithm
+    print("Enter your choice of algorithm")
+    print("1: Uniform Cost Search")
+    print("2: A* with the Misplaced Tile heuristic")
+    print("3: A* with the Eucledian Distance heuristic")
+    amode = ast.literal_eval(input())
+
+    if amode == '1':
+
--- puzzle.py
+++ puzzle.py
@@ -2,26 +2,58 @@ import array
 
 # Define puzzle class
 class Puzzle:
+    # Data memebers
+    num_row = 3
     num_col = 3
-    first_row = array.array([0, 0 , 0])
-    second_row = array.array([0, 0, 0])
-    third_row = array.array([0, 0, 0])
+    blank_row = None
+    blank_col = None
+    initial_state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    table = [[0, 0, 0,], [0, 0, 0], [0, 0, 0]]
+    goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, None]]
+
+    # Build puzzle table using user input
+    def build_custom_table(self, row, col, val):
+        if row < self.num_row and col < self.num_col:
+            self.initial_state[row][col] = val
+            self.table[row][col] = self.initial_state[row][col]
+        return
+    
+    # Operator 1
+    def move_blank_left(self):
+        if self.blank_col > 0:
+            self.table[self.blank_row][self.blank_col], self.table[self.blank_row][self.blank_col-1] = \
+                self.table[self.blank_row][self.blank_col-1], self.table[self.blank_row][self.blank_col]
+            self.blank_col -= 1
+        return
+
+    # Operator 2
+    def move_blank_right(self):
+        if self.blank_col < self.num_col - 1:
+            self.table[self.blank_row][self.blank_col], self.table[self.blank_row][self.blank_col+1] = \
+                self.table[self.blank_row][self.blank_col+1], self.table[self.blank_row][self.blank_col]
+            self.blank_col += 1
+        return
+    
+    # Operator 3
+    def move_blank_up(self):
+        if self.blank_row > 0:
+            self.table[self.blank_row][self.blank_col], self.table[self.blank_row-1][self.blank_col] = \
+                self.table[self.blank_row-1][self.blank_col], self.table[self.blank_row][self.blank_col]
+            self.blank_row -= 1
+        return
+
+    # Operator 4
+    def move_blank_down(self):
+        if self.blank_row < self.num_row - 1:
+            self.table[self.blank_row][self.blank_col], self.table[self.blank_row+1][self.blank_col] = \
+                self.table[self.blank_row+1][self.blank_col], self.table[self.blank_row][self.blank_col]
+            self.blank_row += 1
+        return
 
     def swap(self, row1, col1, row2, col2):
-        temp = None
-        if row1 == 0:
-            if col1 < len(self.num_col):
-                temp = self.first_row[col1]
-            else:
-                return
-        elif row1 == 1:
-            if col1 < len(self.num_col):
-                temp = self.second_row[col1]
-            else:
-                return
-        elif row1 == 2:
-            if col1 < len(self.num_col):
-                temp = self.third_row[col1]
-            else:
-                return
+        if row1 >= self.num_row or row2 >= self.num_row:
+            return
+        if col1 >= self.num_col or row2 >= self.num_col:
+            return
+        self.table[row1][col1], self.table[row2][col2] = self.table[row2][col2], self.table[row1][col1]
         return
